59470449873e421e9d5cc2f499b1fc2c1d8e9900
204G1A0545 <94540546+204G1A0545@users.noreply.github.com>
2022-08-30 11:17:09 +0530

    I Updated Sample.c

--- OSLAB/SAMPLE.C
+++ OSLAB/SAMPLE.C
@@ -1,5 +1,6 @@
-Name Of the Candidate: M.Joshika
+/* Name Of the Candidate: M.Joshika
 Roll No: 204G1A0545
 Title of the Experiment: CPU Scheduling Using FCFS Algorithm
 Date of Creation: 30-08-2022
 Date of Execution: 30-08-2022
+*/
