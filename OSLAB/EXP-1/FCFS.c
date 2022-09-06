5d80c04e2c7bc35866bd396941167e5106abdcbe
204G1A0545 <94540546+204G1A0545@users.noreply.github.com>
2022-09-06 12:02:54 +0530

    Joshika Created fcfs.c

---
+++ OSLAB/EXP-1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate:M.Joshika
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 05-09-2022
+   Date of Execution: 05-09-2022
+*/
