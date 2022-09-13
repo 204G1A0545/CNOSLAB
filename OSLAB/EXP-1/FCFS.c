794fd36d3ee92ac1f440aa4eee4db4d9c81f4e2d
204G1A0545 <94540546+204G1A0545@users.noreply.github.com>
2022-09-13 11:30:03 +0530

    Code Updated

--- OSLAB/EXP-1/FCFS.c
+++ OSLAB/EXP-1/FCFS.c
@@ -5,8 +5,7 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
@@ -31,5 +30,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
