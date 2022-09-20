f87c169d25360e781cdd52376d9cbad266079253
204G1A0545 <204G1A0545@srit.ac.in>
2022-09-20 12:58:04 +0530

    Output s are avaliable in OUTPUT Directory

--- OSLAB/EXP-1/Priority.c
+++ OSLAB/EXP-1/Priority.c
@@ -5,7 +5,7 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-main()
+int main()
 {
 int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; 
 float wtavg,tatavg;
@@ -41,5 +41,4 @@ printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUNDTIME");
 for(i=0;i<n;i++)
 printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d ",p[i],pri[i],bt[i],wt[i],tat[i]);
 printf("\nAverage Waiting Time is --- %f",wtavg/n); printf("\nAverageTurnaround Time is --- %f",tatavg/n);
-getch();
 }
