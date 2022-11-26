60cc9417bdc05d53f3d89f08f50eef64d430f1b9
204G1A0545 <204G1A0545@srit.ac.in>
2022-11-26 15:52:55 +0530

    Exp9 completed Successfully

--- OSLAB/EXP-9/prevent.c
+++ OSLAB/EXP-9/prevent.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-void main()
+int main()
 {
 char job[10][10];
-int time[10],avail,tem[10],temp[10]; 
-int safe[10];
+int time[10],avail,tem[10],temp[10]; int safe[10];
 int ind=1,i,j,q,n,t;
 printf("Enter no of jobs: ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
 printf("Enter name and time: ");
-scanf("%s%d",&job[i],&time[i]);
+scanf("%s%d",&job[i][i],&time[i]);
 }
 printf("Enter the available resources:");
 scanf("%d",&avail);
@@ -50,4 +49,5 @@ printf("No safe sequence\n");
 printf("Safe sequence is:");
 for(i=1;i<ind; i++)
 printf("%s %d\n",job[safe[i]],time[safe[i]]);
+return 0;
 }
